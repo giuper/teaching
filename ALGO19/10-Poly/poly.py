2795c20ee663d9388c75c1d400fe2438c39fef12
Giuseppe Persiano <giuper@gmail.com>
2019-11-26 08:23:48 +0100

    nRegine

--- ALGO19/10-Poly/poly.py
+++ ALGO19/10-Poly/poly.py
@@ -1,4 +1,5 @@
 from lList import lList
+from mergeIter import MergeIter
 
 class Monomial:
 
@@ -9,14 +10,60 @@ class Monomial:
     def __mul__(self,other):
         return Monomial(self.c*other.c,self.e+other.e)
 
+    def __add__(self,other):
+        return Monomial(self.c+other.c,self.e)
+
+    def __eq__(self,other):
+        return self.e==other.e
+
+    def __gt__(self,other):
+        return self.e>other.e
+
+    def __lt__(self,other):
+        return self.e<other.e
+
     def __str__(self):
-        return(str(self.c)+"*x^"+str(self.e))+"+"
+        if self.c==1  and self.e==0:
+                return("+1")
+        if self.c==-1 and self.e==0:
+                return("-1")
+
+        if self.c==1  and self.e==1:
+                return("+x")
+        if self.c==-1 and self.e==1:
+                return("-x")
+
+        if self.c==1 and self.e>1:
+            return("+x^"+str(self.e))
+
+        if self.c==-1 and self.e>1:
+            return("+x^"+str(self.e))
+        
+        if self.c>0:
+            return("+"+str(self.c)+"*x^"+str(self.e))
+        else:
+            return("-"+str(-self.c)+"*x^"+str(self.e))
+
+
+
+class _PolyIterator:
+    def __init__(self,p):
+        self.iter=iter(p.list)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.iter)
 
 
 class Polynomial:
 
         def __init__(self):
             self.list=lList()
+
+        def __iter__(self):
+            return _PolyIterator(self)
 
         def append(self,mono):
             self.list.append(mono)
@@ -25,9 +72,17 @@ class Polynomial:
             self.list.insert(mono)
 
         def __str__(self):
-            return str(self.list)[:-1]
+            return str(self.list)[1:]
         
         def __add__(self,other):
+            zz=MergeIter(self,other)
+            res=Polynomial()
+            for m in zz:
+                if (m.c!=0):
+                    res.append(m)
+            return res
+
+        def __add1__(self,other):
             a=self.list.head
             b=other.list.head
             res=Polynomial()
@@ -59,10 +114,15 @@ class Polynomial:
         def mulByMono(self,m):
             res=Polynomial()
 
-            for n in self.list:
+            for n in self:
                 rm=n*m
                 res.append(rm)
                 
             return res
 
-            
+        def __mul__(self,other):
+            res=Polynomial()
+            for ms in other:
+                temp=self.mulByMono(ms)
+                res=res+temp
+            return res
