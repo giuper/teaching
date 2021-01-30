980f5de14c16cd69c06d5cc140f2cfd8ce371e1a
Giuseppe Persiano <giuper@gmail.com>
2021-01-30 07:38:33 +0100

    Soluzioni esame

---
+++ ALGO20/Exams/210128/SubsetSum-04/driver.py
@@ -0,0 +1,18 @@
+from pm import PM
+
+
+L=[3,7,6,19,2]
+
+print(L)
+
+for t in range(1,45):
+    s=PM(L,t)
+    print("t=",t)
+    if s.Solve():
+        print(s)
+    else:
+        print("Input:\t",L)
+        print("No sol")
+    print()
+
+
