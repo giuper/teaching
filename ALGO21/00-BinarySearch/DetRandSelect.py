34a5413fb602f29b77dd6ef63e612226e9a12ffb
Giuseppe Persiano <giuper@gmail.com>
2021-10-07 10:20:33 +0200

    Det vs Rand

---
+++ ALGO21/00-BinarySearch/DetRandSelect.py
@@ -0,0 +1,74 @@
+import sys
+import random
+import time
+
+def DetSelect(A,k):
+    pivot=A[0] 
+    L=[]
+    R=[]
+    
+    for a in A:
+        if a<pivot:
+            L.append(a)
+        if a>pivot:
+            R.append(a)
+            
+    l=len(L)
+    if l==k:
+        return pivot
+    if l>k:
+        return DetSelect(L,k)
+    else:
+        return DetSelect(R,k-l-1)
+
+def RandSelect(A,k):
+    pivot=random.choice(A)
+    L=[]
+    R=[]
+    
+    for a in A:
+        if a<pivot:
+            L.append(a)
+        if a>pivot:
+            R.append(a)
+            
+    l=len(L)
+    if l==k:
+        return pivot
+    if l>k:
+        return RandSelect(L,k)
+    else:
+        return RandSelect(R,k-l-1)
+    
+
+def driver(N):
+    print(f'{"Lista di lunghezza: "}{N:12d}')
+    A=list(range(N))
+    maxN=sys.getrecursionlimit()-10
+    totalR=0
+    nofRuns=100
+    if N>=maxN:
+        nofRuns=1
+    for i in range(nofRuns):
+        startR=time.time()
+        RandSelect(A,N-1)
+        finishR=time.time()
+        totalR+=finishR-startR
+    averageR=totalR/nofRuns
+    print(f'{"Algoritmo probabilistico: ":30s}{averageR:3.10f}')
+
+    if N<maxN:   
+        startD=time.time()
+        DetSelect(A,N-1)
+        finishD=time.time()
+        print(f'{"Algoritmo deterministico: ":30s}{finishD-startD:3.10f}')
+        print(f'{"Speed-up:":30s}{(finishD-startD)/averageR:3.10f}')
+        
+    print()
+        
+if __name__=='__main__':
+    NN=[100,200,300,400,500,600,700,800,900,1000,2000,10**5,10**6,10**7,10**8,2*10**8]
+
+    for N in NN:
+        driver(N)
+    
