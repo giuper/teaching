60c6a3533eb04a4999dcd831bc2263b2c3642784
Giuseppe Persiano <giuper@gmail.com>
2021-11-20 10:18:39 +0100

    Backtrack

--- ALGO21/07-Backtrack/queen.py
+++ ALGO21/07-Backtrack/queen.py
@@ -1,12 +1,12 @@
 
-##controlla se la posizione della regina nella riga R
-##e' compatibile con le regine nelle righe 0,1,...,R-1
-def checkQueen(posizioni,R):
+##controlla se la colonna C e' una posizione della regina della riga R
+##compatibile con le regine nelle righe 0,1,...,R-1
+def checkQueen(R,regine,C):
     r1=R
-    c1=posizioni[R]
+    c1=C
     for r in range(R):
         r2=r
-        c2=posizioni[r]
+        c2=regine[r]
         if attackingQ(r1,c1,r2,c2):
             return False
     return True
@@ -28,8 +28,8 @@ def solve(R,regine,N):
     if R==N:
         return True
     for C in range(N):
-        regine[R]=C
-        if checkQueen(regine,R):
+        if checkQueen(R,regine,C):
+            regine[R]=C
             if solve(R+1,regine,N):
                 return True
     return False
